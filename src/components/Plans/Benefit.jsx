f26739a4a37c9dfc341a79a0d42f4b20c8643c91
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-09-11 14:24:06 -0300

    Add: styles in Plans

--- src/components/Plans/Benefit.jsx
+++ src/components/Plans/Benefit.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import iconCheck from '../../assets/icons/gl_tick-circle-red.svg'
+import { IconCheck } from '../ui/IconCheck'
 import './Benefit.css'
 
 export default function Benefit ({ benefit }) {
   return (
     <section className='Benefit'>
-      <img src={iconCheck} alt='icon check' />
+      <IconCheck />
       <h5>
         {benefit}
       </h5>
