8dfca9a345d73f623827abe842c84e1e915761df
Ramsés <gz.ramses.oc@gmail.com>
2023-09-15 14:20:22 -0300

    Fix: add images in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,19 +18,19 @@ export function Footer () {
           <p>¿Tienes algo por decirnos? Escríbenos a <a href='mailto:gringolandiaenglish@gmail.com' target='no_blank'>gringolandiaenglish@gmail.com</a></p>
           <div className='Footer-icons'>
             <a className='Footer-a' href='https://www.instagram.com/gringolandiaenglish/' target='no_blank'>
-              <img className='Footer-img' src='src/assets/icons/gl_icon-in.svg' />
+              <img className='Footer-img' src='./src/assets/icons/gl_icon-in.svg' />
             </a>
             <a className='Footer-a' href='https://www.youtube.com/@gringolandiaenglish' target='no_blank'>
-              <img className='Footer-img' src='src/assets/icons/gl_icon-yt.svg' />
+              <img className='Footer-img' src='./src/assets/icons/gl_icon-yt.svg' />
             </a>
             <a className='Footer-a' href='https://www.tiktok.com/@gringolandiaenglish?_t=8fbdw9PkwPF&_r=1' target='no_blank'>
-              <img className='Footer-img' src='src/assets/icons/gl_icon-tt.svg' />
+              <img className='Footer-img' src='./src/assets/icons/gl_icon-tt.svg' />
             </a>
             <a className='Footer-a' href='mailto:gringolandiaenglish@gmail.com' target='no_blank'>
-              <img className='Footer-img' src='src/assets/icons/gl_icon-ml.svg' />
+              <img className='Footer-img' src='./src/assets/icons/gl_icon-ml.svg' />
             </a>
             <a className='Footer-a' href='https://www.facebook.com/profile.php?id=100065104316610' target='no_blank'>
-              <img className='Footer-img' src='src/assets/icons/gl_icon-fb.svg' />
+              <img className='Footer-img' src='./src/assets/icons/gl_icon-fb.svg' />
             </a>
           </div>
         </div>
