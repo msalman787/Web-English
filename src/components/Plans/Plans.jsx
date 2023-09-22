25bd30c3b75109cea25bb35e279f0c04359e1735
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-09-21 23:22:48 -0300

    Add: styles in plans paginator

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Plan from './Plan'
 import { Titles } from '../Titles/Titles'
 import { data } from '../../data/data'
-import './Plans.css'
-import Plan from './Plan'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import './Plans.css'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 export function Plans () {
   const { plans } = data
@@ -18,9 +21,10 @@ export function Plans () {
       <section className='Plans-container'>
         <Swiper
           className='Swiper-plans'
-          slidesPerView={3}
-          spaceBetween={30}
-          slidesPerGroup={4}
+          pagination={{
+            dynamicBullets: true
+          }}
+          modules={[Pagination]}
           breakpoints={{
             300: {
               slidesPerView: 1,
