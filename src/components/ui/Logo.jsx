7446195bdbc3c9920f0de717d162da27c584f10a
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-09-25 13:36:31 -0300

    Add: accesibility web

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -5,7 +5,7 @@ import './Logo.css'
 export function Logo ({ size }) {
   return (
     <picture className='Logo'>
-      <img className={size === 'sm' ? 'max-w-[70px]' : 'max-w-[194px]'} src={logo} alt='Imagén de logo de Gringolandia' />
+      <img width={size === 'sm' ? '70px' : '194px'} height={size === 'sm' ? '70px' : '194px'} className={size === 'sm' ? 'max-w-[70px]' : 'max-w-[194px]'} src={logo} alt='Imagén de logo de Gringolandia' />
     </picture>
   )
 }
