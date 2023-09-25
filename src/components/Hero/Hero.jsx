df0a181c1207820de0128a5c95092835769a3943
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-09-25 14:14:45 -0300

    Edit: properties video

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export function Hero () {
       <div className='Hero-h2'>
         <Titles title='Explora el mundo a través del ingles: Aprende, conecta, crece.' />
       </div>
-      <video className='Hero-video' autoPlay loop defaultMuted playsinline muted aria-label='Video' style='min-width:100%; min-height:100%;'>
+      <video className='Hero-video' autoPlay loop muted aria-label='Video'>
         <source src={videoHero} type='video/mp4' />
         Tu navegador no es compatible con videos HTML5
       </video>
